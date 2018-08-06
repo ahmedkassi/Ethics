6b1cdb53391977a3261af330cef5a4f14ba04146
ahmedkassi <ahmedkassi.junior@gmail.com>
2018-08-07 00:38:28 +0200

    naiveprogress

--- src/main/java/NaivesBayesClassifier.java
+++ src/main/java/NaivesBayesClassifier.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.*;
 
 public class NaivesBayesClassifier {
@@ -6,24 +7,26 @@ public class NaivesBayesClassifier {
     public String companyClass(TreeMap invertedindex,DocumentModel document){
         return null ;
     }
-    public Double companyProbabilityOfaClass(TreeMap<String,ArrayList<Integer>> invertedindex, List<String> bagofwords , int moralclass , List<String> moralclasswords ){
-        Double result =0.0;
+    public BigDecimal companyProbabilityOfaClass(TreeMap<String,ArrayList<Integer>> invertedindex, List<String> bagofwords , int moralclass , List<String> moralclasswords ){
+        BigDecimal  result = new BigDecimal(1.0);
         HashMap<String,Integer> moralclassuniquewords = new HashMap<String, Integer>() ;
         HashSet<String> words = new HashSet<String>(moralclasswords);
+        List<BigDecimal> listOfprobab = new ArrayList<>() ;
         for (String word : words) {
             int occurences = Collections.frequency(moralclasswords,word);
             moralclassuniquewords.put(word,occurences);
         }
         int dominator = moralclassuniquewords.size()+ moralclassuniquewords.values().stream().mapToInt(Integer::intValue).sum();
-        int nominator = 1;
+
         for(String ti : bagofwords){
-
+            Double nominator =1.0;
         if(invertedindex.containsKey(ti)){
+            nominator += invertedindex.get(ti).get(moralclass);
+        }
 
-            nominator +=  invertedindex.get(ti).get(moralclass);
+            listOfprobab.add(new BigDecimal(nominator/dominator));
         }
-        }
-        return null;
+      return null;
 
     }
 }
